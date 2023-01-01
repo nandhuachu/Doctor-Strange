8652f04c2c3c24dbb2515b1dbdda4f421e0bfb50
Venom <67759177+nandhuachu@users.noreply.github.com>
2023-01-01 20:47:53 +0530

    Update covid.py

--- plugins/helper/covid.py
+++ plugins/helper/covid.py
@@ -12,7 +12,7 @@ BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("𝙲𝙻𝙾𝚂𝙴", ca
 async def reply_info(client, message):
     query = message.text.split(None, 1)[1]
     await message.reply_photo(
-        photo="https://telegra.ph/file/1c1ea87828c9f00dc3ab8.jpg",
+        photo="https://te.legra.ph/file/06426c71d17694a356b64.jpg",
         caption=covid_info(query),
         quote=True
     )
